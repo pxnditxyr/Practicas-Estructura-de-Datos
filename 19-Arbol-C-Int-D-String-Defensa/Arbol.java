8d0f2fa4c1a38d0d36cba7c7ad6d5a1b0f613fd8
Yuki xD <pxnditxyr@gmail..com>
2021-05-04 12:05:37 -0400

    Defensa 19

---
+++ 19-Arbol-C-Int-D-String-Defensa/Arbol.java
@@ -0,0 +1,37 @@
+public class Arbol
+{
+	Nodo raiz;
+
+	public Arbol() {
+		raiz = null;
+	}
+	public void inicializar() {
+		raiz = null;
+	}
+
+	public boolean arbolVacio() {
+		return raiz == null;
+	}
+
+	public void insertar(int clave, String dato) {
+
+		Nodo nuevo = new Nodo(clave, dato);
+
+		if( arbolVacio() ) {
+			raiz=nuevo;
+		} else {
+			Nodo p = raiz, q = null;
+			while( p != null ) {
+				q = p;
+				if( p.clave > clave )
+					p = p.hizq;
+				else
+					p = p.hder;
+			}
+			if ( q.clave > clave )
+				q.hizq = nuevo;
+			else
+				q.hder = nuevo;
+		}
+	}
+}
---
+++ 19-Arbol-C-Int-D-String-Defensa/Operaciones.java
@@ -0,0 +1,59 @@
+public class Operaciones {
+
+	public Arbol insertarN( Arbol arbol ) {
+
+		System.out.print( "No de nodos = " );
+		int n = Leer.datoInt();
+		for( int i = 1; i <= n; i++ ) {
+			System.out.println( "Nodo " + i + ": " );
+			System.out.print( "Clave: " );
+			int clave = Leer.datoInt();
+			System.out.print( "Dato: " );
+			String dato = Leer.dato();
+			arbol.insertar(clave, dato);
+		}
+		return arbol;
+	}
+
+	public void preorden( Nodo p ) {
+		if( p != null ) {
+			System.out.println( p.clave + ", " + p.dato );
+			preorden(p.hizq);
+			preorden(p.hder);
+		}
+	}
+
+	public void simetrico( Nodo p ) {
+		if( p != null ) {
+			simetrico( p.hizq );
+			if ( p.clave % 2 != 0 )
+				System.out.println( p.clave + ", " + p.dato );
+			simetrico(p.hder);
+		}
+	}
+
+	public void posorden( Nodo p ) {
+		if( p!=null ) {
+			posorden( p.hizq );
+			posorden( p.hder );
+			if ( p.clave % 2 == 0 )
+				System.out.println( p.clave + ", " + p.dato );
+		}
+	}
+
+	public void buscar( Arbol arbol, int cb )
+	{
+		Nodo p = arbol.raiz;
+		while( p != null && p.clave != cb ) {
+			if( p.clave > cb )
+				p = p.hizq;
+			else
+				p = p.hder;
+			}
+			if ( p == null )
+				System.out.println("NO LOCALIZADO");
+			else
+				System.out.println(p.dato);
+	}
+}
+
