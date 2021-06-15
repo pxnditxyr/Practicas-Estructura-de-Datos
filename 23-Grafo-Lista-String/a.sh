41d016990943ec93726bf233ccb1adc4f7aa19ab
Yuki xD <pxnditxyr@gmail..com>
2021-06-15 11:31:44 -0400

    24-Grafo

---
+++ 23-Grafo-Lista-String/a.sh
@@ -0,0 +1,4 @@
+find . -name "*.class" -type f -delete
+javac Principal.java
+java Principal
+find . -name "*.class" -type f -delete
---
+++ 24-Grafos-Lista-String/NodoG.java
@@ -0,0 +1,18 @@
+class NodoG{
+
+    String datox;
+    NodoG sigx;
+    Nodo vec;
+
+    public NodoG() {
+        datox = null;
+        sigx = null;
+        vec=null;
+    }
+
+    public NodoG(String d) {
+        datox = d;
+        sigx = null;
+        vec = null;
+    }
+}
