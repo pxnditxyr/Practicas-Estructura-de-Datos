040cdafd3107e41349ae3d46f9b0e2e798a87d34
Yuki xD <pxnditxyr@gmail..com>
2021-06-24 10:09:26 -0400

    Exam Final

--- 23-Grafo-Lista-String/a.sh
+++
@@ -1,4 +0,0 @@
-find . -name "*.class" -type f -delete
-javac Principal.java
-java Principal
-find . -name "*.class" -type f -delete
---
+++ 28-Grafo-Lista-String-Examen/NodoD.java
@@ -0,0 +1,14 @@
+class NodoD{
+
+    String dato;
+    NodoD sig,ant;
+
+    public NodoD() {
+        dato = null;
+        sig = ant = null;
+    }
+    public NodoD(String d) {
+        dato = d;
+        sig = ant = null;
+    }
+}
